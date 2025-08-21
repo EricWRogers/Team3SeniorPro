783d42dda5c2a583e748a400205c70fb6619105c
Dylan Hilliard <ghilliard69420@gmail.com>
2025-08-21 17:36:26 -0500

    Basic random Spawner setup for meteors

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject[] objectToSpawn;
+
+    private GameObject[] pooledObjects;
+    public int poolSize = 10;
+    public float spawnInterval = 2f;
+    private float nextSpawnTime;
+    public Vector3 spawnAreaSize = new Vector3(50f, 10f, 50f);
+  
+
+
+    void Update()
+    {
+            if (Time.time >= nextSpawnTime)
+            {
+                SpawnObject();
+                nextSpawnTime = Time.time + spawnInterval;
+            }
+    }
+
+    void OawGizmos()
+    {
+        
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, spawnAreaSize);
+    }
+
+    void SpawnObject(){
+
+        int randIndex = Random.Range(0, objectToSpawn.Length);
+        Vector3 spawnPosition = new Vector3(
+            Random.Range(-spawnAreaSize.x / 2, spawnAreaSize.x / 2),
+            Random.Range(-spawnAreaSize.y / 2, spawnAreaSize.y / 2),
+            Random.Range(-spawnAreaSize.z / 2, spawnAreaSize.z / 2)
+        ) + transform.position;
+
+        Instantiate(objectToSpawn[randIndex], spawnPosition, Quaternion.identity);
+    }
+}

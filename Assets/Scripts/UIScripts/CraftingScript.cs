1d1ada4e112b5936e0d6de4160775c47c2a2a445
Garrett Shipp <garwad9@gmail.com>
2025-08-25 11:25:24 -0500

    Getting pull

--- Assets/Scripts/UIScripts/CraftingScript.cs
+++ Assets/Scripts/UIScripts/CraftingScript.cs
@@ -12,16 +12,17 @@ public class CraftingScript : MonoBehaviour
 
     public void Craft(RecipeRef _recipe)
     {
-        Debug.Log(CheckForingredints(_recipe));
+        Debug.Log(CheckForingredints(_recipe, out List<InventorySlot> _invSlot));
 
 
     }
 
-    public bool CheckForingredints(RecipeRef _recipeToCheck)
+    public bool CheckForingredints(RecipeRef _recipeToCheck, out List<InventorySlot> _invSlot)
     {
         m_requiredItems = (recipes.allRecipes[_recipeToCheck.recipe]);
         List<ItemData> distinctItemList = m_requiredItems.Distinct<ItemData>().ToList();
         PseudoDictionary<ItemData, int> requiredItemDict = new PseudoDictionary<ItemData, int>();
+        _invSlot = new();
 
         foreach (ItemData item in distinctItemList)
         {
@@ -30,12 +31,15 @@ public class CraftingScript : MonoBehaviour
         foreach (ItemData item in distinctItemList)
         {
             int itemCounter = requiredItemDict[item];
-            for (int x = 0; x < playerInventory.PrimaryInventorySystem.inventorySize; x++)
+            for (int x = playerInventory.PrimaryInventorySystem.inventorySize - 1; x > -1; x--)
             {
-                if (playerInventory.PrimaryInventorySystem.InventorySlots[x].ItemData == item)
+                if (playerInventory.PrimaryInventorySystem.InventorySlots[x].ItemData == item && itemCounter >= 0)
                 {
+                    _invSlot.Add(playerInventory.PrimaryInventorySystem.InventorySlots[x]);
                     itemCounter -= playerInventory.PrimaryInventorySystem.InventorySlots[x].StackSize;
+                    //Debug.Log($"inventory slot {x} has {playerInventory.PrimaryInventorySystem.InventorySlots[x].StackSize} {playerInventory.PrimaryInventorySystem.InventorySlots[x].ItemData} ");
                 }
+                
             }
             if (itemCounter > 0)
             {

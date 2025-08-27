3ae8641819b56e7218668080c9ad33b780c563fd
dylanHilliard <ghilliard69420@gmail.com>
2025-08-27 17:21:05 -0500

    Added passive ai

---
+++ Assets/Scripts/Enemy/MobSpawner.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobSpawner : MonoBehaviour
+{
+
+    public List<GameObject> mobsToSpawn;
+    public Vector3 spawnArea = new Vector3(20, 10, 20);
+
+    public int numberOfMobs = 10;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

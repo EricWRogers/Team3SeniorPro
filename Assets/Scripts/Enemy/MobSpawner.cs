9f880fa093da08890d1e8197cc044aee31bbacaf
Dylan Hilliard <ghilliard69420@gmail.com>
2025-08-27 22:52:25 -0500

    Aded item drops to ai and made mob spawner

--- Assets/Scripts/Enemy/MobSpawner.cs
+++ Assets/Scripts/Enemy/MobSpawner.cs
@@ -5,14 +5,27 @@ public class MobSpawner : MonoBehaviour
 {
 
     public List<GameObject> mobsToSpawn;
-    public Vector3 spawnArea = new Vector3(20, 10, 20);
+    public Vector3 spawnArea = new Vector3(100, 40, 100);
 
     public int numberOfMobs = 10;
 
 
+
     void Start()
     {
-        
+       for (int i = 0; i < numberOfMobs; i++)
+        {
+            Vector3 randomPosition = new Vector3(
+                Random.Range(-spawnArea.x / 2, spawnArea.x / 2),
+                Random.Range(-spawnArea.y / 2, spawnArea.y / 2),
+                Random.Range(-spawnArea.z / 2, spawnArea.z / 2)
+            );
+
+            Vector3 spawnPosition = transform.position + randomPosition;
+
+            int mobIndex = Random.Range(0, mobsToSpawn.Count);
+            Instantiate(mobsToSpawn[mobIndex], spawnPosition, Quaternion.identity);
+        }
     }
 
     // Update is called once per frame

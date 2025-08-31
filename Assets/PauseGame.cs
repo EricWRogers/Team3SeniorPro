74e9cbffcb18fc2e6d0b2b316f4005bc7d2d7985
Garrett <garwad9@gmail.com>
2025-08-31 18:10:22 -0500

    PauseMenu

---
+++ Assets/PauseGame.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+
+public class PauseGame : MonoBehaviour
+{
+    public bool gamePause;
+    public GameObject PauseHUD;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gamePause = false;
+        Time.timeScale = 1;
+        PauseHUD.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+
+        }
+    }
+
+    public void TogglePause()
+    {
+        if (!gamePause)
+        {
+            GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().canMove = false;
+            Cursor.lockState = CursorLockMode.None;
+            gamePause = true;
+            PauseHUD.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().canMove = true;
+            Cursor.lockState = CursorLockMode.Locked;
+            gamePause = false;
+            Time.timeScale = 1;
+            PauseHUD.SetActive(false);
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
